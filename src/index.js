66bfc2f2d4bcb72c89989301bdfb7b93d8118d67
María García Antúnez <maria.garcia.antunez@gmail.com>
2025-03-15 12:40:39 +0100

    Set up first endpoint

--- src/index.js
+++ src/index.js
@@ -2,6 +2,25 @@
 
 const express = require('express');
 const cors = require('cors');
+require("dotenv").config();
+const mysql = require('mysql2/promise');
+
+async function getConnection() {
+    const connectionData = {
+        host: process.env["MYSQL_HOST"],
+        port: process.env["MYSQL_PORT"],
+        user: process.env["MYSQL_USER"],
+        password: process.env["MYSQL_PASS"],
+        database: process.env["MYSQL_SCHEMA"],
+    };
+
+    const connection = await mysql.createConnection(connectionData);
+    await connection.connect();
+
+    return connection;
+}
+
+// Configuración del servidor
 const app = express();
 
 app.use(cors());
@@ -15,15 +34,28 @@ app.listen(port, () => {
 
 
 // Primer endpoint
-app.get('/categorias', function (req, res) {
+app.get('/categorias', async (req, res) => {
+    try {
 
-    const results = [];
-    const numOfElements = results.length;
+        // Obtener conexión
+        const conn = await getConnection();
 
-    res.json(
-        {
-            info: { "count": numOfElements },
+        // Ejecutar la consulta
+        const [results] = await conn.query(`SELECT * FROM categorias;`);
+
+        // Calcular la cantidad de elementos
+        const numOfElements = results.length;
+
+        // Cerrar la conexión después de la consulta
+        await conn.end();
+
+        // Responder con los resultados y el número de elementos
+        res.json({
+            info: { count: numOfElements },
             results: results,
-        }
-    );
+        });
+    } catch (error) {
+        console.error(error); // Log de error
+        res.status(500).json({ error: 'Hubo un problema al obtener las categorías.' });
+    }
 });
